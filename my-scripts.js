84c933a461cd651a65baf73d93997fc5fdf76b4f
Nicholas Guyaux <nicholas.guyaux@gmail.com>
2019-02-15 15:25:44 -0700

    Add files via upload

--- my-scripts.js
+++ my-scripts.js
@@ -1,29 +1,43 @@
 // store data
-var myList = (JSON.parse(localStorage.getItem('myData')) || [])
+var myList = (JSON.parse(localStorage.getItem('myData')) || []);
 
-// create items
+// create list items
 function createItem() {
-  var newItem = {
-    text: window.prompt('Your new item prompt goes here'),
-    style: 'none'
-  } myList.push(newItem)
-  localStorage.setItem('myData', JSON.stringify(myList))
-  printItem(newItem)
+  // create movie list item
+  var newItem = {text: window.prompt('Add a movie to the list'), style: 'none'};
+
+  // add item to array
+  myList.push(newItem);
+
+  // sync array with database
+  localStorage.setItem('myData', JSON.stringify(myList));
+
+  // call printItem
+  printItem(newItem);
 }
 
-// print items
+// print item
 function printItem(item) {
-  var p = document.createElement('p')
-  p.innerHTML = item.text
-  p.className = item.style
-  p.onclick = function() {
+  // create <p> element
+  var p = document.createElement('p');
+  // add text to <p>
+  p.innerHTML = item.text;
+  // add style to <p>
+  p.className = item.style;
+  // add click functionality
+  p.onclick =
+      function() {
     this.className = item.style =
-        (this.className == 'none') ? 'strike' : 'invisible'
-    localStorage.setItem('myData', JSON.stringify(myList))
-  } document.getElementById('myDiv').appendChild(p)
+        (this.className == 'none') ? 'strike' : 'invisible';
+    // sync array with database
+    localStorage.setItem('myData', JSON.stringify(myList));
+  }
+      // add <p> to myDiv
+      document.getElementById('myDiv')
+          .appendChild(p);
 }
 
 // loop and print list
-for (x = 0; x < myList.length; x++) {
-  printItem(myList[x])
-}
+myList.forEach(element => {
+  printItem(element);
+});
